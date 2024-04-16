56bad6f997ae96ee22f6cd8944f63468c9e05605
Aimee <aimee.maroney@intapp.com>
2024-04-16 16:01:55 -0400

    first commit

---
+++ src/components/tds-button.ts
@@ -0,0 +1,7 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+@customElement('tds-button')
+export class TdsButton extends LitElement {
+
+}
