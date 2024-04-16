2547f48aca6f076f321b305d04acba5552e8efe1
Aimee <aimee.maroney@intapp.com>
2024-04-16 17:14:25 -0400

    add button

--- src/components/tds-button.ts
+++ src/components/tds-button.ts
@@ -4,4 +4,12 @@ import { customElement, property } from 'lit/decorators.js'
 @customElement('tds-button')
 export class TdsButton extends LitElement {
 
+    @property({ type: String })
+    label = ''
+
+    render() {
+        return html`
+            <button>${this.label}</button>
+        `
+    }
 }
