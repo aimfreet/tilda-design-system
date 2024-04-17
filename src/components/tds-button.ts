2042ceb8221af7454f57b9c4dd0bef8034c072c0
Aimee <aimee.maroney@intapp.com>
2024-04-17 12:30:05 -0400

    add button variants

--- src/components/tds-button.ts
+++ src/components/tds-button.ts
@@ -1,15 +1,63 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
+type ButtonVariant = "primary" | "secondary" | "outline" | "ghost";
+
 @customElement('tds-button')
 export class TdsButton extends LitElement {
 
+    @property({ type: String, attribute: "button-variant" })
+    color: ButtonVariant = 'primary';
+
+    @property({ type: Boolean, attribute: "button-disabled" })
+    disabled = false;
+    
     @property({ type: String })
-    label = ''
+        label = ''
 
     render() {
         return html`
-            <button>${this.label}</button>
+            <button class="btn ${this.color}" ?disabled="${this.disabled}">${this.label}</button>
         `
     }
+
+    static get styles() {
+        return [
+          css`
+            .btn {
+              border-radius: 10px;
+              color: #fff;
+              cursor: pointer;
+              padding: 0.5rem;
+            }
+    
+            button[disabled] {
+              cursor: not-allowed;
+            }
+    
+            .primary {
+              color: #000;
+              background: PapayaWhip;
+            }
+    
+            .secondary {
+              background: rebeccapurple;
+            }
+    
+            .danger {
+              background: tomato;
+            }
+    
+            .info {
+              color: #000;
+              background: skyblue;
+            }
+    
+            .success {
+              color: #000;
+              background: MediumAquamarine;
+            }
+          `
+        ];
+      }
 }
